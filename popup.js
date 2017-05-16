50371775573d184c54f3939287256d8923e38547
nyculescu <cn.cata@gmail.com>
2017-05-17 00:02:19 +0300

    facebook like & share button implemented but not working

--- popup.js
+++ popup.js
@@ -96,3 +96,20 @@ document.addEventListener('DOMContentLoaded', function() {
     var status = 'Welcome to NCC Bet';
     renderStatus(status);
 });
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: '264058520732059',
+        xfbml: true,
+        version: 'v2.9'
+    });
+    FB.AppEvents.logPageView();
+};
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=1144334365586207";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
